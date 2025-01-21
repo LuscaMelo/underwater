b2b069d9b885e5d404b93f617b900ea5eb925c83
Lucas Melo <lucasmelo.desenvolvedor@gmail.com>
2025-01-21 11:59:11 -0300

    [FEAT] Adding dynamic background with scroll

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,45 +2,74 @@
 
 import Image from 'next/image'
 import Link from 'next/link'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { IoMdMenu } from 'react-icons/io'
 import { IoClose } from 'react-icons/io5'
 
 function Navbar() {
     const navLinks = ['Home', 'About Us', 'Contact Us', 'Packages', 'Destination']
     const [menuOpen, setMenuOpen] = useState(false)
+    const [scrolled, setScrolled] = useState(false)
 
     function handleMenu() {
-        setMenuOpen(!menuOpen)
+        setMenuOpen((prev) => {
+            const newState = !prev
+            if (newState) {
+                document.body.classList.add('overflow-hidden') // Adiciona overflow-hidden
+            } else {
+                document.body.classList.remove('overflow-hidden') // Remove overflow-hidden
+            }
+            return newState
+        })
     }
 
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 180) {
+                setScrolled(true)
+            } else {
+                setScrolled(false)
+            }
+        }
+
+        window.addEventListener('scroll', handleScroll)
+        return () => {
+            window.removeEventListener('scroll', handleScroll)
+            document.body.classList.remove('overflow-hidden') // Garante que a classe é removida ao desmontar o componente
+        }
+    }, [])
+
     return (
-        <div className="sticky w-full bg-transparent h-[70px] z-[99]">
-            <div className='max-w-[1400px] mx-auto px-3 h-full flex items-center justify-between'>
+        <div className={`sticky top-0 w-full z-[99] duration-300 ${scrolled ? 'bg-white shadow-lg' : 'bg-transparent'}`}>
+            <div className='max-w-[1400px] mx-auto px-3 h-[70px] flex items-center justify-between'>
                 <Link href='/'>
-                    <Image className='brightness-[10000%]' src='./logoipsum.svg' alt='Underwater logo' width={150} height={40} quality={100} />
+                    <Image className={`brightness-[10000%] ${scrolled ? 'filter-none' : ''}`} src='./logoipsum.svg' alt='Underwater logo' width={150} height={40} quality={100} />
                 </Link>
 
                 <div className='lg:flex items-center gap-3 lg:gap-8 hidden'>
                     <nav>
-                        <ul className='flex gap-5 text-white'>
+                        <ul className={`flex gap-5 ${scrolled ? 'text-black' : 'text-white'}`}>
                             {navLinks.map((link, index) => (
                                 <li key={index}>
                                     <Link href='/'>
-                                        <span className='font-medium border-b-2 border-transparent hover:border-primary duration-300'>{link}</span>
+                                        <span className={`font-medium border-b-2 border-transparent hover:border-primary duration-300`}>
+                                            {link}
+                                        </span>
                                     </Link>
                                 </li>
                             ))}
                         </ul>
                     </nav>
 
-                    <button className='px-5 py-1 bg-transparent border border-white font-medium hover:bg-white text-white hover:text-black duration-200 rounded-full'>Call Us</button>
+                    <button className={`px-5 py-1 bg-transparent border font-medium duration-200 rounded-full ${scrolled ? 'border-primary text-primary hover:bg-primary hover:text-white' : 'border-white text-white hover:bg-white hover:text-black'}`}>
+                        Call Us
+                    </button>
                 </div>
 
                 <div className={`lg:hidden ${menuOpen ? 'hidden' : ''}`}>
                     <button
                         onClick={handleMenu}
-                        className='px-2 py-1 text-2xl bg-transparent border text-white border-white font-medium hover:bg-white hover:text-black duration-200 rounded-xl'>
+                        className={`px-2 py-1 text-2xl bg-transparent border font-medium duration-200 rounded-xl ${scrolled ? 'border-primary text-primary hover:bg-primary hover:text-white' : 'border-white text-white hover:bg-white hover:text-black'}`}>
                         <IoMdMenu />
                     </button>
                 </div>
@@ -48,7 +77,7 @@ function Navbar() {
 
             {/* Menu para dispositivos móveis */}
             <div
-                className={`${menuOpen ? 'left-0 md:right-0' : 'left-[-100vw] md:right-[-100vw]'} absolute flex items-center justify-center top-0 duration-300 w-screen md:w-[50%] h-screen bg-primary overflow-hidden lg:hidden z-[50]`}>
+                className={`${menuOpen ? 'left-0 md:right-0' : 'left-[-100vw] md:right-[-100vw]'} absolute flex items-center justify-center top-0 duration-300 w-screen md:w-[50%] max-h-[100vh] h-screen bg-primary overflow-hidden lg:hidden z-[50]`}>
                 <button
                     onClick={handleMenu}
                     className='absolute top-3 right-3 text-3xl text-white'>
@@ -63,7 +92,7 @@ function Navbar() {
                         {navLinks.map((link, index) => (
                             <li key={index}>
                                 <Link href='/'>
-                                    <span className='font-medium text-2xl text-white border-b-2 border-transparent hover:border-primary duration-300'>{link}</span>
+                                    <span className='font-medium text-2xl text-white border-b-2 border-transparent hover:border-primary duration-300 hover:text-dark'>{link}</span>
                                 </Link>
                             </li>
                         ))}
@@ -74,7 +103,7 @@ function Navbar() {
             {/* Overlay */}
             <div
                 onClick={handleMenu}
-                className={`absolute top-0 w-screen h-screen ${menuOpen ? 'my-blur' : 'hidden'} transition-opacity duration-300 lg:hidden`}
+                className={`absolute top-0 w-screen h-screen ${menuOpen ? 'bg-black/50' : 'hidden'} transition-opacity duration-300 lg:hidden`}
             ></div>
         </div >
     )
