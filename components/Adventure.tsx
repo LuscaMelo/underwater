e3f9b0b1ec32115bfee690bb359ddd77ef7b12d5
Lucas Melo <lucasmelo.desenvolvedor@gmail.com>
2025-01-21 18:18:38 -0300

    [FIX] Creating modal functionality for the carousel on Adventure

--- components/Adventure.tsx
+++ components/Adventure.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useState, useEffect } from "react";
 import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
@@ -8,8 +9,12 @@ import "swiper/css/pagination";
 
 import { Navigation } from "swiper/modules";
 import { GrFormNext, GrFormPrevious } from "react-icons/gr";
+import { AiOutlineClose } from "react-icons/ai";
 
 function Adventure() {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [selectedImage, setSelectedImage] = useState<string | null>(null);
+
     const images = [
         '/adventure-1.png',
         '/adventure-2.png',
@@ -21,8 +26,32 @@ function Adventure() {
         '/adventure-8.png',
     ];
 
+    const openModal = (src: string) => {
+        setSelectedImage(src);
+        setIsModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setSelectedImage(null);
+        setIsModalOpen(false);
+    };
+
+    // Disable scroll when the modal is open
+    useEffect(() => {
+        if (isModalOpen) {
+            document.body.style.overflow = "hidden";
+        } else {
+            document.body.style.overflow = "auto";
+        }
+
+        // Clean up when the component is unmounted or modal state changes
+        return () => {
+            document.body.style.overflow = "auto";
+        };
+    }, [isModalOpen]);
+
     return (
-        <section className="bg-primary text-white py-14 lg:pb-20">
+        <section className="bg-primary text-white pb-14 lg:pb-20">
             <div className="max-w-[1400px] mx-auto px-3">
                 <div className="flex gap-2 mb-5 items-center">
                     <div className="h-12 lg:h-14 w-12 lg:w-14 p-2 bg-white bg-opacity-10 rounded-xl">
@@ -49,7 +78,11 @@ function Adventure() {
                     className="mt-10"
                 >
                     {images.map((src, index) => (
-                        <SwiperSlide key={index} className="h-[250px] lg:h-[300px] rounded-2xl overflow-hidden">
+                        <SwiperSlide
+                            key={index}
+                            className="h-[250px] lg:h-[300px] rounded-2xl overflow-hidden cursor-pointer"
+                            onClick={() => openModal(src)}
+                        >
                             <Image
                                 src={src}
                                 alt={`Slide image ${index + 1}`}
@@ -72,6 +105,34 @@ function Adventure() {
                     </button>
                 </div>
             </div>
+
+            {/* Modal */}
+            {isModalOpen && selectedImage && (
+                <div
+                    className="fixed inset-0 z-[999] my-blur bg-black bg-opacity-80 flex items-center justify-center"
+                    onClick={closeModal}
+                >
+                    <div
+                        className="relative w-[83%] md:w-[50%] lg:w-[32%] 2xl:w-[28%]"
+                        onClick={(e) => e.stopPropagation()}
+                    >
+                        <button
+                            className="absolute top-3 right-3 text-primary hover:text-dark duration-300 hover:brightness-110 p-2 bg-white rounded-full shadow-xl text-xl"
+                            onClick={closeModal}
+                        >
+                            <AiOutlineClose />
+                        </button>
+                        <Image
+                            src={selectedImage}
+                            alt="Selected image on carousel"
+                            width={1200}
+                            height={800}
+                            className="w-full h-full object-contain rounded-2xl pointer-events-none"
+                            quality={100}
+                        />
+                    </div>
+                </div>
+            )}
         </section>
     );
 }
